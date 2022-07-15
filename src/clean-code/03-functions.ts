d677edc96b9e892237763afdf93f0540a82c6dac
altorresi <alejandro.torres@itconsultores.com.co>
2022-07-15 16:17:22 -0500

    SESSION 3.1 functions logic refactor

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -78,4 +78,38 @@
     console.log("Crear actor");
     return true;
   }
+
+  // COMPLICATED FUNCTIONS
+  const getPayAmount = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false,
+  }) => {
+    let result;
+    if (isDead) {
+      result = 1500;
+    } else {
+      if (isSeparated) {
+        result = 2500;
+      } else {
+        if (isRetired) {
+          result = 3000;
+        } else {
+          result = 4000;
+        }
+      }
+    }
+
+    return result;
+  };
+
+  const getPayAmountRefactored = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false,
+  }) => {
+    if (isDead) return 1500;
+    if (isSeparated) return 2500;
+    return isRetired ? 3000 : 4000;
+  };
 })();
