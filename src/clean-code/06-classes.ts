5821e7f0e9f6e82d844007cb57eadf3cf3d911a5
altorresi <alejandro.torres@itconsultores.com.co>
2022-07-22 16:14:42 -0500

    object like properties on classes

--- src/clean-code/06-classes.ts
+++ src/clean-code/06-classes.ts
@@ -1,30 +1,42 @@
 (() => {
   type Gender = "M" | "F";
 
+  interface PersonProps {
+    name: string;
+    gender: Gender;
+    birthdate: Date;
+  }
 
   class Person {
     public name: string;
     public gender: Gender;
     public birthdate: Date;
 
-    constructor(_name: string, _gender: Gender, _birthdate: Date) {
-      this.name = _name;
-      this.gender = _gender;
-      this.birthdate = _birthdate;
+    constructor({ name, gender, birthdate }: PersonProps) {
+      this.name = name;
+      this.gender = gender;
+      this.birthdate = birthdate;
     }
+  }
+
+  interface UserProps {
+    email: string;
+    role: string;
+    name: string;
+    gender: Gender;
+    birthdate: Date;
   }
 
   class User extends Person {
     public lastAccess: Date;
-    constructor(
-      public email: string,
-      public role: string,
-      name: string,
-      gender: Gender,
-      birthdate: Date
-    ) {
-      super(name, gender, birthdate);
+    public email: string;
+    public role: string;
+
+    constructor({ email, role, name, gender, birthdate }: UserProps) {
+      super({ name, gender, birthdate });
       this.lastAccess = new Date();
+      this.email = email;
+      this.role = role;
     }
 
     checkCredentials() {
@@ -32,32 +44,50 @@
     }
   }
 
+  interface UserSettingsProps {
+    workingDirectory: string;
+    lastOpenFolder: string;
+    email: string;
+    role: string;
+    name: string;
+    gender: Gender;
+    birthdate: Date;
+  }
+
   class UserSettings extends User {
-    constructor(
-      public workingDirectory: string,
-      public lastOpenFolder: string,
-      email: string,
-      role: string,
-      name: string,
-      gender: Gender,
-      birthdate: Date
-    ) {
-      super(email, role, name, gender, birthdate);
+    public workingDirectory: string;
+    public lastOpenFolder: string;
+    constructor({
+      workingDirectory,
+      lastOpenFolder,
+      email,
+      role,
+      name,
+      gender,
+      birthdate,
+    }: UserSettingsProps) {
+      super({ email, role, name, gender, birthdate });
+      this.workingDirectory = workingDirectory;
+      this.lastOpenFolder = lastOpenFolder;
     }
   }
 
-  const person = new Person("Alejandro", "M", new Date("2000-01-28"));
+  const person = new Person({
+    name: "Alejandro",
+    gender: "M",
+    birthdate: new Date("2000-01-28"),
+  });
   console.log({ person });
 
-  const userSettings = new UserSettings(
-    "/user/home",
-    "/home",
-    "altovoy@gmail.com",
-    "Admin",
-    "Alejandro Torres",
-    "M",
-    new Date("2000-01-28")
-  );
+  const userSettings = new UserSettings({
+    workingDirectory: "/user/home",
+    lastOpenFolder: "/home",
+    email: "altovoy@gmail.com",
+    role: "Admin",
+    name: "Alejandro Torres",
+    gender: "M",
+    birthdate: new Date("2000-01-28"),
+  });
 
   console.log({ userSettings });
 })();
