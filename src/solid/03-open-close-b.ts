2bc4d4117e467f3da83905c434349b531170baa0
altovoy-ayte <alejandro.torres@itconsultores.com.co>
2022-07-30 20:39:03 -0500

    SOLID open/close apply

--- src/solid/03-open-close-b.ts
+++ src/solid/03-open-close-b.ts
@@ -1,8 +1,9 @@
-import axios from "axios";
+import { HttpClient } from "./03-open-close-c";
 
 export class TodoService {
+  constructor(private http: HttpClient) {}
   async getTodoItems() {
-    const { data } = await axios.get(
+    const { data } = await this.http.get(
       "https://jsonplaceholder.typicode.com/todos/"
     );
     return data;
@@ -10,8 +11,9 @@ export class TodoService {
 }
 
 export class PostService {
+  constructor(private http: HttpClient) {}
   async getPosts() {
-    const { data } = await axios.get(
+    const { data } = await this.http.get(
       "https://jsonplaceholder.typicode.com/posts"
     );
     return data;
@@ -19,8 +21,9 @@ export class PostService {
 }
 
 export class PhotosService {
+  constructor(private http: HttpClient) {}
   async getPhotos() {
-    const { data } = await axios.get(
+    const { data } = await this.http.get(
       "https://jsonplaceholder.typicode.com/photos"
     );
     return data;
