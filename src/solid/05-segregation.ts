24f138ee72bbf42523a51b481b100b9fb62dd6f3
altovoy-ayte <alejandro.torres@itconsultores.com.co>
2022-07-30 21:19:35 -0500

    SOLID interface segregation - solving problem

--- src/solid/05-segregation.ts
+++ src/solid/05-segregation.ts
@@ -1,42 +1,43 @@
 interface Bird {
-  fly(): void;
   eat(): void;
+}
+
+interface FlyerBird {
+  fly(): void;
+}
+
+interface RunnerBird {
   run(): void;
+}
+
+interface SwimmerBird {
   swim(): void;
 }
 
-class Tucan implements Bird {
-  swim(): void {
-    throw new Error("This bird can't swim");
-  }
+class Tucan implements Bird, FlyerBird {
   public fly() {}
   public eat() {}
-  public run() {}
 }
 
-class Humminbird implements Bird {
-  swim(): void {
-    throw new Error("This bird can't swim");
-  }
+class Humminbird implements Bird, FlyerBird {
   public fly() {}
   public eat() {}
-  public run() {}
 }
 
-class Ostrich implements Bird {
-  swim(): void {
-    throw new Error("This bird can't swim");
-  }
-  fly(): void {
-    throw new Error("This bird can't fly");
-  }
+class Ostrich implements Bird, RunnerBird {
   eat(): void {}
   run(): void {}
 }
 
-class Penguin implements Bird {
-  fly(): void {}
+class Penguin implements Bird, RunnerBird, SwimmerBird {
   eat(): void {}
   run(): void {}
   swim(): void {}
 }
+
+/*
+Detectar violación del principio de segregación de interfaz
+
++ Si las interfaces que diseñamos nos obligan a violar los principios de responsabilidad única y substitución de Liskov
+
+*/
