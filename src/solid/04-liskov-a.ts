cb8554832ff4f2165d69bb2175568f978b8ad3a9
altovoy-ayte <alejandro.torres@itconsultores.com.co>
2022-07-30 21:06:00 -0500

    SOLID liskov substitution  - applying

--- src/solid/04-liskov-a.ts
+++ src/solid/04-liskov-a.ts
@@ -1,29 +1,12 @@
-import { Tesla, Audi, Toyota, Honda, Volvo } from "./04-liskov-b";
+import { Tesla, Audi, Toyota, Honda, Volvo, Ford } from "./04-liskov-b";
 
 (() => {
-  const printCarSeats = (cars: (Tesla | Audi | Toyota | Honda | Volvo)[]) => {
-    for (const car of cars) {
-      if (car instanceof Tesla) {
-        console.log("Tesla", car.getNumberOfTeslaSeats());
-        continue;
-      }
-      if (car instanceof Audi) {
-        console.log("Audi", car.getNumberOfAudiSeats());
-        continue;
-      }
-      if (car instanceof Toyota) {
-        console.log("Toyota", car.getNumberOfToyotaSeats());
-        continue;
-      }
-      if (car instanceof Honda) {
-        console.log("Honda", car.getNumberOfHondaSeats());
-        continue;
-      }
-      if (car instanceof Volvo) {
-        console.log("Volvo", car.getNumberOfVolvoSeats());
-        return;
-      }
-    }
+  const printCarSeats = (
+    cars: (Tesla | Audi | Toyota | Honda | Volvo | Ford)[]
+  ) => {
+    cars.forEach((car) => {
+      console.log(car.constructor.name, car.getNumberOfSeats());
+    });
   };
 
   const cars = [
@@ -32,6 +15,7 @@ import { Tesla, Audi, Toyota, Honda, Volvo } from "./04-liskov-b";
     new Toyota(5),
     new Honda(5),
     new Volvo(7),
+    new Ford(3),
   ];
 
   printCarSeats(cars);
