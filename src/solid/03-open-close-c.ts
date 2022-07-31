34b2efaf55de8367cb5956d398b6317c5f8d3c4a
altovoy-ayte <alejandro.torres@itconsultores.com.co>
2022-07-30 20:44:50 -0500

    SOLID open/close -  remove axios dependency

--- src/solid/03-open-close-c.ts
+++ src/solid/03-open-close-c.ts
@@ -1,9 +1,19 @@
-import axios from "axios";
-
 export class HttpClient {
   async get(url: string) {
-    const { data, status } = await axios.get(url);
+    const response = await fetch(url);
+    const data = response.json();
+    const status = response.status;
     console.log({ status });
     return { data, status };
   }
 }
+
+/*
+
+Cómo detectar violaciones de OPC
+
++ Cambios normalmente afectan nuestra clase o módulo
+
++ Cuando una clase o módulo afecta muchas capas (Presentación, almacenamiento, etc)
+
+*/
